eca7dc62756e50e01d205b968c574e29d7b27358
evgeni <gg1786@rambler.ru>
2019-07-04 22:00:35 +0200

    Changes with config

--- src/main/java/com/web/controller/StudentController.java
+++ src/main/java/com/web/controller/StudentController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StudentController {
 
+
     @GetMapping("/message")
     public String message() {
         return "showMessage";
     }
-
 }
